7512893db49c90895e47884d0ec515e3fd206606
Steve Jo <tedjasuryastevenjonathan@gmail.com>
2022-07-13 10:37:08 +0700

    add testing

--- calc_test.go
+++ calc_test.go
@@ -3,46 +3,37 @@ package main
 import (
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCalculator_Add(t *testing.T) {
-	t.Run("Calculator Add operator testing", func(t *testing.T) {
-		numSample1 := 10
-		numSample2 := 20
 
+	myCalc := Calculator{Num1:4, Num2:4}
 
-		calc := Calculator{
-			Num1: numSample1,
-			Num2: numSample2,
-		}
+	actual := myCalc.Add()
 
-		if calc.Add() != 30 {
-			t.Error("It should return 30")
-		}
+	expected := 8
 
-	})
+	assert.Equal(t, actual, expected , "it should return 8")
+
 }
 
 func TestCalculator_Sub(t *testing.T) {
-	t.Run("Calculator Sub operator testing", func(t *testing.T) {
-		numSample1 := 3
-		numSample2 := 2
 
+	myCalc := Calculator{Num1:5, Num2:4}
 
-		calc := Calculator{
-			Num1: numSample1,
-			Num2: numSample2,
-		}
+	actual := myCalc.Sub()
 
-		if calc.Sub() != 1 {
-			t.Error("It should return 1")
-		}
+	expected := 1
 
-	})
+	assert.Equal(t, actual, expected , "it should return 1")
+
 }
 
 func TestMainApp(t *testing.T) {
 	os.Args = []string{"-num1", "2", "-num2", "3", "-opr", "add"}
 	main()
+
 }
 
